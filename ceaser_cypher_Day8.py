ae2e4ba6e3a9070fb20054937601e0a75a8c78ab
Aayush Kumar <kumaraayush2804@gmail.com>
2024-09-17 23:59:24 -0400

    Update ceaser_cypher_Day8.py

--- ceaser_cypher_Day8.py
+++ ceaser_cypher_Day8.py
@@ -1,6 +1,6 @@
 
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+print("CEASER CYPHER")
 def caesar(original_text, shift_amount, direction):
     new_message = ""
     for char in original_text:
