454604f1f8cc4ead24db2f0b16da93a2653d1986
Aayush Kumar <kumaraayush2804@gmail.com>
2024-09-20 02:15:32 -0400

    Update Secret_Auction_Day9.py

--- Secret_Auction_Day9.py
+++ Secret_Auction_Day9.py
@@ -3,28 +3,31 @@ import os
 auct_dict = {}
 
 max_bid = 0
+max_bidder = ''
+
+def auction(bidder_name, bid_ammount):
+    global max_bid, max_bidder
+    auct_dict[bidder_name] = bid_ammount
+    if bid_ammount > max_bid:
+        max_bid =bid_ammount
+        max_bidder = bidder_name
+
+def clear_screen(more_bidders):
+    if more_bidders == "yes":
+        os.system('cls')
+
+
 
 go_on = True
 while go_on:
+    name_of_bidder = input("Enter the bidder name: ")
+    ammount_bid = int(input("Enter your bid: "))
+    auction(name_of_bidder,ammount_bid)
 
-    bidder_name = input("Enter the bidder name: ")
-    bid_ammount = int(input("Enter your bid: "))
+    num_of_bidder = input("Do you have anyone else to bid: ").lower()
+    clear_screen(num_of_bidder)
 
-    auct_dict[bidder_name] = bid_ammount
-
-    if bid_ammount > max_bid:
-        max_bid =bid_ammount
-
-    for item in auct_dict:
-        if auct_dict[item] == max_bid:
-            max_bidder = item
-
-    more_bidders = input("Do you have anyone else to bid: ").lower()
-    if more_bidders == "yes":
-        os.system('cls')
-    if more_bidders == "no" :
+    if num_of_bidder == "no":
         go_on = False
 
-
-# print(auct_dict)
-print(f'{max_bidder} won and the highest bid is {max_bid}.')
+print(f'{max_bidder} won and the highest bid is ${max_bid}.')
