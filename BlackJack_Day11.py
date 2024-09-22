d9dcd72c384909cf11d8d0f4649eabe97be87c9a
Aayush Kumar <kumaraayush2804@gmail.com>
2024-09-22 15:44:26 -0400

    Create BlackJack_Day11.py

---
+++ BlackJack_Day11.py
@@ -0,0 +1,67 @@
+import random
+import os
+
+
+list_of_cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10,10,10]
+
+
+def add_card(user_cards, comp_cards):
+    for i in range(2):
+        user_cards.append(random.choice(list_of_cards))
+        comp_cards.append(random.choice(list_of_cards))
+
+    print(f"Your cards are : {user_cards}")
+    # print(f' Comp cards are : {comp_cards}')
+    print(f' Comp cards are : {comp_cards[:-1]}')
+
+def more_cards(user_cards, comp_cards):
+    more_cards_need = True
+    while more_cards_need:
+        user_more_cards = input("Enter 'y' to get more cards and 'n' to pass: ").lower()
+        if user_more_cards == 'y':
+            user_cards.append(random.choice(list_of_cards))
+            comp_cards.append(random.choice(list_of_cards))
+            print(f"User cards: {user_cards},Computer Cards:  {comp_cards[:-1]}")
+
+            user_sum = sum(user_cards)
+            comp_sum = sum(comp_cards)
+            if user_sum > 21 or comp_sum > 21:
+                more_cards_need = False
+ 
+        if user_more_cards == 'n':
+            more_cards_need = False
+
+    
+def calculate_scores(user_cards, comp_cards):
+    user_sum = sum(user_cards)
+    comp_sum = sum(comp_cards)
+    if user_sum > 21:
+        print(f"You lose. You sum is {user_sum} and is over 21.")
+    elif comp_sum > 21:
+        print(f"Comp lose. Comp sum is {comp_sum} and is over 21.")
+    elif user_sum == comp_sum:
+        print("Its a tie...")
+    elif user_sum > comp_sum:
+        print(f"You won. your sum is {user_sum} anc comp sum is {comp_sum}")
+    else:
+        print(f"You lose. your sum is {user_sum} anc comp sum is {comp_sum}")
+    
+
+def main():
+    go_on = True
+    while go_on:
+        user_cards = []
+        comp_cards = []
+        add_card(user_cards, comp_cards)
+        more_cards(user_cards, comp_cards)
+        calculate_scores(user_cards, comp_cards)
+        play_again = input("Wanna play again: 'yes' or 'no' ").lower()
+        if play_again == "no":
+            go_on = False
+        else:
+            os.system('cls')
+
+
+
+
+main()
