ddab93a0eb266c6381f933e8e6b6ec054080be36
Aayush Kumar <kumaraayush2804@gmail.com>
2024-09-22 21:45:59 -0400

    Create Number_guess_game_Day12.py

---
+++ Number_guess_game_Day12.py
@@ -0,0 +1,48 @@
+import random
+import os
+print("welcom to Number Guessing Game")
+
+def num_to_guess():
+    num_to_guess = random.randint(1,100)
+    print(num_to_guess)
+
+
+def guess(attempt):
+    go_on = True
+    while go_on:
+        guess_input = int(input("Enter your guess : "))
+        if guess_input == num_to_guess:
+            print("Correct")
+            break
+        elif guess_input > num_to_guess:
+            print("Too high")
+            attempt -=1
+        else:
+            print("Too low")
+            attempt -=1
+        print(f'You have {attempt} attempts left.')
+        if attempt == 0:
+            go_on = False
+ 
+
+
+def main():
+    play_again = True
+    while play_again:
+        num_to_guess()
+        difficulty = input("Type 'easy' for easy mode and 'hard' for hard mode: ").lower()
+        if difficulty == "easy":
+            attempt = 10
+        if difficulty == "hard":
+            attempt = 5
+        guess(attempt)
+
+        wanna_play_again = input(f"Want to play again 'yes or no': ").lower()
+        if wanna_play_again != "no":
+            os.system('cls')
+        else:
+            play_again = False
+            
+
+main()
+
