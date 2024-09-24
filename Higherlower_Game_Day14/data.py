e9203ab0cfb8db5e8705b5e77cb4a8f887d1397d
Aayush Kumar <kumaraayush2804@gmail.com>
2024-09-24 00:09:32 -0400

    Create data.py

---
+++ Higherlower_Game_Day14/data.py
@@ -0,0 +1,279 @@
+data_insta = [
+    {
+        "name": "Instagram",
+        "follower_count": 650_000_000,
+        "description": "The official account of Instagram.",
+        "country": "Global"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 605_000_000,
+        "description": "Portuguese footballer and athlete.",
+        "country": "Portugal"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 485_000_000,
+        "description": "Argentine footballer, currently playing for Inter Miami.",
+        "country": "Argentina"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 430_000_000,
+        "description": "Singer, actress, and producer.",
+        "country": "USA"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 400_000_000,
+        "description": "Model, entrepreneur, and media personality.",
+        "country": "USA"
+    },
+    {
+        "name": "Dwayne 'The Rock' Johnson",
+        "follower_count": 395_000_000,
+        "description": "Actor and former professional wrestler.",
+        "country": "USA"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 390_000_000,
+        "description": "Singer, songwriter, and actress.",
+        "country": "USA"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 385_000_000,
+        "description": "Entrepreneur and media personality.",
+        "country": "USA"
+    },
+    {
+        "name": "Beyoncé",
+        "follower_count": 330_000_000,
+        "description": "Singer, songwriter, and actress.",
+        "country": "USA"
+    },
+    {
+        "name": "Khloe Kardashian",
+        "follower_count": 310_000_000,
+        "description": "Media personality and entrepreneur.",
+        "country": "USA"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 290_000_000,
+        "description": "Singer and songwriter.",
+        "country": "Canada"
+    },
+    {
+        "name": "Nike",
+        "follower_count": 280_000_000,
+        "description": "Sportswear and equipment brand.",
+        "country": "USA"
+    },
+    {
+        "name": "National Geographic",
+        "follower_count": 255_000_000,
+        "description": "Nature, science, and culture magazine.",
+        "country": "USA"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 250_000_000,
+        "description": "Singer, songwriter, and producer.",
+        "country": "USA"
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 250_000_000,
+        "description": "Indian cricketer.",
+        "country": "India"
+    },
+    {
+        "name": "Neymar Jr.",
+        "follower_count": 250_000_000,
+        "description": "Brazilian footballer.",
+        "country": "Brazil"
+    },
+    {
+        "name": "Zendaya",
+        "follower_count": 180_000_000,
+        "description": "Actress and singer.",
+        "country": "USA"
+    },
+    {
+        "name": "Cardi B",
+        "follower_count": 180_000_000,
+        "description": "Rapper and songwriter.",
+        "country": "USA"
+    },
+    {
+        "name": "Miley Cyrus",
+        "follower_count": 170_000_000,
+        "description": "Singer and actress.",
+        "country": "USA"
+    },
+    {
+        "name": "Kourtney Kardashian",
+        "follower_count": 170_000_000,
+        "description": "Media personality and entrepreneur.",
+        "country": "USA"
+    },
+    {
+        "name": "FC Barcelona",
+        "follower_count": 155_000_000,
+        "description": "Spanish football club.",
+        "country": "Spain"
+    },
+    {
+        "name": "Real Madrid",
+        "follower_count": 155_000_000,
+        "description": "Spanish football club.",
+        "country": "Spain"
+    },
+    {
+        "name": "Shakira",
+        "follower_count": 140_000_000,
+        "description": "Singer and songwriter.",
+        "country": "Colombia"
+    },
+    {
+        "name": "LeBron James",
+        "follower_count": 140_000_000,
+        "description": "Professional basketball player.",
+        "country": "USA"
+    },
+    {
+        "name": "Billie Eilish",
+        "follower_count": 140_000_000,
+        "description": "Singer and songwriter.",
+        "country": "USA"
+    },
+    {
+        "name": "Chris Brown",
+        "follower_count": 130_000_000,
+        "description": "Singer and songwriter.",
+        "country": "USA"
+    },
+    {
+        "name": "Vin Diesel",
+        "follower_count": 120_000_000,
+        "description": "Actor and producer.",
+        "country": "USA"
+    },
+    {
+        "name": "Rihanna",
+        "follower_count": 120_000_000,
+        "description": "Singer, entrepreneur, and actress.",
+        "country": "Barbados"
+    },
+    {
+        "name": "Emma Watson",
+        "follower_count": 110_000_000,
+        "description": "Actress and activist.",
+        "country": "UK"
+    },
+    {
+        "name": "Dua Lipa",
+        "follower_count": 110_000_000,
+        "description": "Singer and songwriter.",
+        "country": "UK"
+    },
+    {
+        "name": "David Beckham",
+        "follower_count": 110_000_000,
+        "description": "Retired footballer and entrepreneur.",
+        "country": "UK"
+    },
+    {
+        "name": "NASA",
+        "follower_count": 100_000_000,
+        "description": "National Aeronautics and Space Administration.",
+        "country": "USA"
+    },
+    {
+        "name": "Priyanka Chopra",
+        "follower_count": 90_000_000,
+        "description": "Actress and producer.",
+        "country": "India"
+    },
+    {
+        "name": "Shawn Mendes",
+        "follower_count": 90_000_000,
+        "description": "Singer and songwriter.",
+        "country": "Canada"
+    },
+    {
+        "name": "Gal Gadot",
+        "follower_count": 90_000_000,
+        "description": "Actress and producer.",
+        "country": "Israel"
+    },
+    {
+        "name": "Gigi Hadid",
+        "follower_count": 85_000_000,
+        "description": "Model and media personality.",
+        "country": "USA"
+    },
+    {
+        "name": "Huda Kattan",
+        "follower_count": 85_000_000,
+        "description": "Makeup artist and entrepreneur.",
+        "country": "USA"
+    },
+    {
+        "name": "Snoop Dogg",
+        "follower_count": 85_000_000,
+        "description": "Rapper and entrepreneur.",
+        "country": "USA"
+    },
+    {
+        "name": "Zlatan Ibrahimović",
+        "follower_count": 80_000_000,
+        "description": "Swedish footballer.",
+        "country": "Sweden"
+    },
+    {
+        "name": "PSG",
+        "follower_count": 80_000_000,
+        "description": "French football club.",
+        "country": "France"
+    },
+    {
+        "name": "Conor McGregor",
+        "follower_count": 75_000_000,
+        "description": "MMA fighter and entrepreneur.",
+        "country": "Ireland"
+    },
+    {
+        "name": "Virgil Abloh",
+        "follower_count": 70_000_000,
+        "description": "Fashion designer.",
+        "country": "USA"
+    },
+    {
+        "name": "Lewis Hamilton",
+        "follower_count": 70_000_000,
+        "description": "Formula One racing driver.",
+        "country": "UK"
+    },
+    {
+        "name": "Zendaya",
+        "follower_count": 70_000_000,
+        "description": "Actress and singer.",
+        "country": "USA"
+    },
+    {
+        "name": "Kevin Hart",
+        "follower_count": 70_000_000,
+        "description": "Comedian and actor.",
+        "country": "USA"
+    },
+    {
+        "name": "Jennifer Lopez",
+        "follower_count": 60_000_000,
+        "description": "Singer, actress, and entrepreneur.",
+        "country": "USA"
+    },
+   
+]
