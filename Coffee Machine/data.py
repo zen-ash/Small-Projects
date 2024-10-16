56a69a9de82069d03a4343be30227fad08186cd6
Aayush Kumar <kumaraayush2804@gmail.com>
2024-10-16 10:36:50 -0400

    Create data.py

---
+++ Coffee Machine/data.py
@@ -0,0 +1,34 @@
+coffee_info = {
+    "espresso" :{
+        "ingredients" : {
+        "water" : 50,
+        "coffee" : 18,
+        "milk" : 0
+    },
+    "cost" : 1.5
+    },
+
+    "latte" : {
+        "ingredients" : {
+        "water" : 200,
+        "coffee" : 24,
+        "milk" : 150
+    },
+    "cost" : 2.5
+    },
+    "cappuccino" : {
+    "ingredients" : {
+        "water" : 250,
+        "coffee" : 24,
+        "milk" : 100
+    },
+    "cost": 3.0
+    }
+}
+
+profit = 0
+resources = {
+    "water" : 300,
+    "milk" : 200,
+    "coffee" : 100 
+}
