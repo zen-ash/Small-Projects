71274c4de295a76e2343eced59224f612fe05d45
Aayush Kumar <kumaraayush2804@gmail.com>
2025-05-21 14:00:25 -0400

    Update main.py

---
+++ DAY 18 - TURTLE/main.py
@@ -0,0 +1,62 @@
+from turtle import Turtle, Screen
+
+import random
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+
+# for i in range(4):
+#     timmy.forward(100)
+#     timmy.right(90)
+
+
+# for i in range(10):
+#     timmy.forward(20)
+#     timmy.penup()
+#     timmy.forward(10)
+#     timmy.pendown()
+
+
+# SIDE = 3
+# for i in range(8):
+#     total_angle = 360
+#     SIDE
+#     angle_of_shape = total_angle/SIDE
+#     a = (random.random())
+#     b = (random.random())
+#     c = (random.random()) 
+#     # print(a,b,c)
+
+#     timmy.pencolor(a,b,c)
+    
+#     for i in range(SIDE):
+#         timmy.forward(100)
+#         timmy.right(angle_of_shape)
+#     SIDE += 1
+
+
+
+def draw_shape(num_of_sides):
+    angle = 360/num_of_sides
+    for i in range(num_of_sides):
+        timmy.forward(100)
+        timmy.right(angle)
+
+def change_color():
+    a = random.random()
+    b = random.random()
+    c = random.random()
+    timmy.pencolor(a,b,c)
+
+def main():
+    num_of_sides = 3
+    while (num_of_sides<=10):
+        change_color()
+        draw_shape(num_of_sides)
+        num_of_sides += 1
+
+main()
+
+screen = Screen()
+screen.exitonclick()
