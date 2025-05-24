734570a8160ca1571b2db9af372115c961e12786
Aayush Kumar <kumaraayush2804@gmail.com>
2025-05-24 00:04:36 -0400

    Day 18

--- DAY 18 - TURTLE/main.py
+++ DAY 18 - TURTLE/main.py
@@ -1,9 +1,14 @@
-from turtle import Turtle, Screen
-
+# from turtle import Turtle, 
+import turtle
+import colors
 import random
 
-timmy = Turtle()
+turtle.colormode(255)
+timmy = turtle.Turtle()
 timmy.shape("triangle")
+timmy.speed('fastest')
+timmy.hideturtle()
+# timmy.pensize(10)
 # timmy.color("red")
 
 # for i in range(4):
@@ -43,11 +48,13 @@ timmy.shape("triangle")
 #         timmy.forward(100)
 #         timmy.right(angle)
 
-def change_color():
-    a = random.random()
-    b = random.random()
-    c = random.random()
-    timmy.pencolor(a,b,c)
+# def change_color():
+#     r = random.random()
+#     g = random.random()
+#     b = random.random()
+#     random_color = (r,g,b)
+#     # timmy.pencolor(r,g,b)
+#     return random_color
 
 # def main():
 #     num_of_sides = 3
@@ -61,27 +68,60 @@ def change_color():
 ####Random Walk
 
 
-def move_forward():
-    timmy.forward(25)
+# def move_forward():
+#     timmy.forward(30)
 
-angle = [0,90,180,270]
-timmy.speed('fast')
-timmy.pensize('5')
+# angle = [0,90,180,270]
 
-
-while True:
-    random_angle = random.choice(angle)
-    change_color()
-    timmy.setheading(random_angle)
-    move_forward()
+# while True:
+#     random_angle = random.choice(angle)
+#     timmy.pencolor(change_color())
+#     # change_color()
+#     timmy.setheading(random_angle)
+#     move_forward()
 
 
+#####SPirograph
+# def draw_spirograph(size_of_gap):
+#     for i in range(int(360/size_of_gap)):
+#         timmy.pencolor(change_color())
+#         timmy.circle(100)
+#         timmy.setheading(timmy.heading() + size_of_gap)
 
 
 
+# a = True
+# while a:
+#     timmy.pencolor(change_color())
+#     timmy.circle(100)
+#     timmy.setheading(timmy.heading() + 5)
+#     print(timmy.heading())
+#     if timmy.heading() == 0.0:
+#         a = False
+
+# draw_spirograph(36)
 
 
 
+##################################painting heirs
 
-screen = Screen()
+timmy.teleport(-300,-300)
+y_cord = -250
+
+for i in range(10):
+    for i in range(10):
+        timmy.color(random.choice(colors.color_list))
+        timmy.begin_fill()
+        timmy.circle(20)
+        timmy.end_fill()
+        timmy.penup()
+        timmy.forward(50)
+        timmy.pendown()
+    timmy.teleport(x=-300, y= y_cord)
+    y_cord += 50
+
+
+
+    
+screen = turtle.Screen()
 screen.exitonclick()
